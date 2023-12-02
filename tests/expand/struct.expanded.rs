81cd7f913513e73f6204827ac55972c6e63b1e2d
Jonathan Giddy <jongiddy@gmail.com>
2023-12-02 15:00:51 +0000

    Add struct test

---
+++ tests/expand/struct.expanded.rs
@@ -0,0 +1,113 @@
+use typle::typle;
+type TupleSequenceOutput1<T0> where T0: Extract = (Option<T0::Output>,);
+type TupleSequenceOutput2<T0, T1>
+where
+    T0: Extract,
+    T1: Extract,
+= (Option<T0::Output>, Option<T1::Output>);
+struct SeqIntoIter1<T0>
+where
+    T0: Into<ByteStream>,
+{
+    t: TupleSequenceOutput1<T0>,
+}
+struct SeqIntoIter2<T0, T1>
+where
+    T0: Into<ByteStream>,
+    T1: Into<ByteStream>,
+{
+    t: TupleSequenceOutput2<T0, T1>,
+}
+pub enum State0 {
+    #[default]
+    Invalid,
+}
+#[automatically_derived]
+impl ::core::default::Default for State0 {
+    #[inline]
+    fn default() -> State0 {
+        Self::Invalid
+    }
+}
+pub enum State1<T0>
+where
+    T0: std::io::Read,
+{
+    #[default]
+    Invalid,
+    S0([Vec<u8>; 0]),
+}
+#[automatically_derived]
+impl<T0> ::core::default::Default for State1<T0>
+where
+    T0: std::io::Read,
+{
+    #[inline]
+    fn default() -> State1<T0> {
+        Self::Invalid
+    }
+}
+pub enum State2<T0, T1>
+where
+    T0: std::io::Read,
+    T1: std::io::Read,
+{
+    #[default]
+    Invalid,
+    S0([Vec<u8>; 0]),
+    S1([Vec<u8>; 1]),
+}
+#[automatically_derived]
+impl<T0, T1> ::core::default::Default for State2<T0, T1>
+where
+    T0: std::io::Read,
+    T1: std::io::Read,
+{
+    #[inline]
+    fn default() -> State2<T0, T1> {
+        Self::Invalid
+    }
+}
+struct AStruct<T> {
+    t: T,
+    state: State<(T)>,
+}
+impl std::io::Read for AStruct0 {
+    fn read(&mut self, mut buf: &mut [u8]) -> std::io::Result<usize> {
+        {}
+    }
+}
+impl<T0> std::io::Read for AStruct1<T0>
+where
+    T0: std::io::Read,
+{
+    fn read(&mut self, mut buf: &mut [u8]) -> std::io::Result<usize> {
+        {
+            {
+                if let State1::S0(output) = self.state.take() {
+                    let size = self.t.0.read(buf);
+                }
+            }
+        }
+    }
+}
+impl<T0, T1> std::io::Read for AStruct2<T0, T1>
+where
+    T0: std::io::Read,
+    T1: std::io::Read,
+{
+    fn read(&mut self, mut buf: &mut [u8]) -> std::io::Result<usize> {
+        {
+            {
+                if let State2::S0(output) = self.state.take() {
+                    let size = self.t.0.read(buf);
+                }
+            }
+            {
+                if let State2::S1(output) = self.state.take() {
+                    let size = self.t.1.read(buf);
+                }
+            }
+        }
+    }
+}
---
+++ tests/expand/struct.rs
@@ -0,0 +1,46 @@
+use typle::typle;
+
+#[typle(Tuple for 1..=2)]
+type TupleSequenceOutput<T>
+where
+    T: Tuple<impl Extract>,
+= typle_expand!(Option<T<{INDEX}>::Output>);
+
+#[typle(Tuple for 1..=2)]
+struct SeqIntoIter<T>
+where
+    T: Tuple<impl Into<ByteStream>>,
+{
+    t: TupleSequenceOutput<(T)>,
+}
+
+#[typle(Tuple for 0..=2)]
+#[derive(Default)]
+pub enum State<T>
+where
+    T: Tuple<impl std::io::Read>,
+{
+    #[default]
+    Invalid,
+    S([Vec<u8>; INDEX]) = typle_variants!(),
+}
+
+#[typle(Tuple for 0..=2)]
+struct AStruct<T> {
+    t: T,
+    state: State<(T)>,
+}
+
+#[typle(Tuple for 0..=2)]
+impl<T> std::io::Read for AStruct<(T)>
+where
+    T: Tuple<impl std::io::Read>,
+{
+    fn read(&mut self, mut buf: &mut [u8]) -> std::io::Result<usize> {
+        for typle_const!(i) in 0..T::LEN {
+            if let State::<typle_index!(T::LEN)>::S::<typle_index!(i)>(output) = self.state.take() {
+                let size = self.t[[i]].read(buf);
+            }
+        }
+    }
+}
